a96ce7965c1c0dd04c2869c08d6684be10808310
huangxiaolin <huangxiaolinc@163.com>
2022-01-18 10:05:52 +0800

    fix: image bundle

--- Example/PhotoAlbumLib/ViewController.swift
+++ Example/PhotoAlbumLib/ViewController.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import PhotoAlbumLib
 
 class ViewController: UIViewController {
 
+    let photoAlbumManager = PhotoAlbumManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let but = UIButton(type: .custom)
+        but.addTarget(self, action: #selector(photoClick), for: .touchUpInside)
+        but.frame = CGRect(x: 100, y: 400, width: 100, height: 50)
+        but.backgroundColor = .red
+        view.addSubview(but)
+        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func photoClick() {
+        let photoConfig = PhotoConfiguration()
+        photoConfig.chatConfiguration()
+        photoAlbumManager.presentPhotoAlbum(controller: self, photoConfig: photoConfig) { [weak self] models, original in
+        }
     }
-
 }
 
